d1bdd966ce95c6fb23db19fe8b67f51b57b2e307
elecumbelly <elecumbelly@gmail.com>
2026-01-08 21:01:16 +0000

    refactor: remove CampaignSelector and switch to full-width calculator grid

--- odetoerlang/src/App.tsx
+++ odetoerlang/src/App.tsx
@@ -418,22 +418,15 @@ function App() {
         {/* Tab Content */}
         <div className="animate-fade-in" role="tabpanel" id={`panel-${activeTab}`}>
           {activeTab === 'calculator' && (
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
-              {/* Left Sidebar - Data Management */}
-              <div className="lg:col-span-1 space-y-4">
-                <CampaignSelector />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <div>
+                <StaffingModelPanel />
               </div>
-              {/* Main Calculator Area */}
-              <div className="lg:col-span-3 grid grid-cols-1 md:grid-cols-3 gap-4">
-                <div>
-                  <StaffingModelPanel />
-                </div>
-                <div>
-                  <InputPanel />
-                </div>
-                <div>
-                  <ResultsDisplay />
-                </div>
+              <div>
+                <InputPanel />
+              </div>
+              <div>
+                <ResultsDisplay />
               </div>
             </div>
           )}
